ae417832ffb624fedf7e32d3e97e1182ec589646
youboll <pedro.ciclorasil@gmail.com>
2021-10-12 14:04:24 -0300

    + Arquivos estaticos

--- views/compra.js
+++ views/compra.js
@@ -1,46 +1,26 @@
 const router = require('express').Router();
-const userData = require('./user.js').userJWT;
-const JWT = require('jsonwebtoken');
-const express = require('express');
-const db = require('../db.js');
-router.use(express.urlencoded({ extended: false}));
-//Adicionar produto
-router.get('/addCarrinho/:id_produto', (req,res) => {
-    let user = new userData();
-    let userInfo = user.getJWtByCookie(req);
-    if (req.params.id_produto == undefined) {res.redirect('/');return(0)}
-    if (req.query.quantidade == undefined) {console.log("Quantidade");res.redirect('/'+req.params.id_produto);return(0)}
-    if (userInfo == false) {res.redirect('/');return(0);}
-    let cookies = user.breakCookie(req.headers.cookie);
-    let cartCookie = {produtos:[]};
-    //Adding empty obj
-    if (cookies['cart'] != undefined && cookies['cart'] != "") {cartCookie = JWT.verify(cookies['cart'],require('../index.js').JWTPrivateKey)}
-    
-    let sql = "SELECT * FROM `remedios` WHERE `cod_remedio` = "+req.params.id_produto+"";
-    db.query(sql, (error,results) => {
-        if (error) {throw error}
-        if (results[0] == undefined) {console.log("Nao encontrado");res.redirect(('/'+req.params.id_produto));return(0)}
-        results[0]['quantidade'] = req.query.quantidade;
-        console.log(results[0])
-        cartCookie.produtos.push(results[0]);
-        let cartJWT = JWT.sign(cartCookie,require('../index.js').JWTPrivateKey)
-        res.cookie('cart',cartJWT);
-        res.send("ok")
-    })
-})
-//Terminar ROTA
-router.get('/removeCarrinho/:id_produto')
+const carrinho = require('./carrinho').carrinho;
+const check_req = require('./signup.js').check_req;
 
-router.get('/carrinho',(req,res) => {
+router.get('/compra/confirmaCompra', (req,res) => {
+    let cart = new carrinho(req).getData();
+    if (cart == false) {res.redirect('/');return(0);}
+    let frete = 0;
+    let valor = 0;
+    for (var x=0;x<cart.produtos.length;x++) {
+        valor += parseFloat(cart.produtos[x].valor * cart.produtos[x].quantidade);
+        frete += parseFloat(cart.produtos[x].valorFrete);
+    }
+    res.render('confirmaCompra.html',{"valor":valor,"frete":frete})
+})
+
+router.post('/compra/registraPedido',(req,res) => {
+    let wanted_info = ['rua','numero','cep','bairro','cidade','estado'];
+    let check_info = check_req(wanted_info,req);
+    if (check_info == false) {
+        res.redirect('/compra/confirmaCompra');return(0)
+    }
+    let cart = new carrinho(req).getData();
     
-    let user = new userData();
-    let userInfo = user.getJWtByCookie(req);
-    console.log("Usuario ",userInfo)
-    if (userInfo == false) {console.log(userInfo);res.redirect('/');return(0);}
-    let cookies = user.breakCookie(req.headers.cookie);
-    
-    let cartData =(userData != undefined)? JWT.verify(cookies['cart'],require('../index.js').JWTPrivateKey) : undefined;
-    
-    res.render('carrinho.html',{"cartData":cartData})
 })
 module.exports = router;
